efbed34474d562f4ba2aa9e92ece2fda8ac4e798
Nicholas Thompson <Gnarlitronic@gmail.com>
2020-07-05 10:25:45 -0700

    basic combat functions 1

--- js/gameplay.js
+++ js/gameplay.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var enemy;
     var enemies = [
         {
-            name: "goblin",
+            name: "Goblin",
             health : 10,
             currentHealth : 10,
             attackPower : 1,
@@ -11,7 +11,7 @@ $(document).ready(function () {
             speed : 3
         },
         {
-            name : "troll",
+            name : "Troll",
             health : 30,
             currentHealth: 30,
             attackPower : 4,
@@ -19,7 +19,7 @@ $(document).ready(function () {
             speed : 0
         },
         {
-            name : "dragon",
+            name : "Dragon",
             health : 100,
             currentHealth : 100,
             attackPower : 20,
@@ -84,15 +84,40 @@ $(document).ready(function () {
     $("#readyGame").click(function(){
         setUp(enemies);
         console.log(enemy.name);
+        enemy.currentHealth = enemy.health;
+        playerStats.player.currentHealth = playerStats.player.maxHealth;
         $("#enemyType").text(enemy.name);
         $("#enemyHealth").text(enemy.currentHealth);
         $("#playerName").text(playerStats.player.name);
         $("#banana").text(playerStats.player.currentMana);
         $("#playerHealth").text(playerStats.player.currentHealth);
+        $("#enemyAttackPower").text(enemy.attackPower);
     });
 
     $("#weaponAttack").click(function (){
-        
+        if ($("#weaponChoice").val() == "sword") {
+        enemy.currentHealth = enemy.currentHealth - 
+        (playerStats.player.attackPower +
+         weapons.sword.attackModifier);
+            $("#enemyHealth").text(enemy.currentHealth);
+        } else if ($("#weaponChoice").val() == "axe") {
+            enemy.currentHealth = enemy.currentHealth - 
+        (playerStats.player.attackPower +
+         weapons.axe.attackModifier);
+         $("#enemyHealth").text(enemy.currentHealth);
+        } else if ($("#weaponChoice").val() == "spear") {
+            enemy.currentHealth = enemy.currentHealth - 
+        (playerStats.player.attackPower +
+         weapons.spear.attackModifier);
+         $("#enemyHealth").text(enemy.currentHealth);
+        } else {
+            alert("Please choose a weapon to attack with");
+        }
+        if (enemy.currentHealth <= 0) {
+            $("#enemyHealth").text("0");
+            alert("You defeated the "+enemy.name);
+        }
+
     })
     
     function setUp () {
